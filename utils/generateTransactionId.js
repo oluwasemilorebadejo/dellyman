d9dcb5ac8a2f60750301b90fc03742e685a08ce7
blac <badejo.semilore.7@gmail.com>
2023-06-04 17:51:40 -0500

    created transactionID function

---
+++ utils/generateTransactionId.js
@@ -0,0 +1,16 @@
+function generateTransactionId() {
+  const prefix = "dellyman-tx-";
+  const length = 10;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let transactionId = prefix;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    transactionId += characters.charAt(randomIndex);
+  }
+
+  return transactionId;
+}
+
+module.exports = generateTransactionId;
