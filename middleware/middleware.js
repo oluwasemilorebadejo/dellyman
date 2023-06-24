00357a22814e7fb9c5f51ab978f7154a104a2b20
blac <badejo.semilore.7@gmail.com>
2023-06-24 01:10:13 +0100

    moved protect function

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -5,40 +5,7 @@ const User = require("../company/models/companyModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-exports.protect = catchAsync(async (req, res, next) => {
-  // get the token and check if it exists
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
 
-  if (!token) {
-    return next(
-      new AppError("you arent logged in. kindly log in to get access", 401)
-    );
-  }
-  // verify token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-  // check if user still exists
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser) {
-    return next(new AppError("the user with this token no longer exists", 401));
-  }
-  // check if user changed password after token was issued
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(new AppError("user changed password. pls log in again", 401));
-  }
-  // grant access to protected route
-  req.user = currentUser; // req.user stores the user data and is only available on protected routes, ie routes that have the protect middleware
-
-  next();
-});
 
 exports.restrictTo =
   (...roles) =>
