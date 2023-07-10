46e2ab703052392f79525e8918395ed37ecbbd54
blac <badejo.semilore.7@gmail.com>
2023-07-10 16:02:33 +0100

    changed email sender

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: "Dellyman",
+    from: "Rider's Institute",
     to: options.email,
     subject: options.subject,
     text: options.message,
